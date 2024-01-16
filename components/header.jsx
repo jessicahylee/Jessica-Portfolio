acdb020c5b860fb60f17dd15c349363053efdc16
jessicahylee <jehyerinn@gmail.com>
2024-01-15 19:38:33 -0500

    Header ready

--- components/header.jsx
+++ components/header.jsx
@@ -1,21 +1,17 @@
 import './header.scss'
+import './partials/fonts.scss'
 
 function Header() {
   return (
     <div className="header__container-div">
       <div className="header__container-logo">
-     <h3 className="header__container-image" > Jessica Lee </h3>
+        <h3 className="header__container-image"> Jessica Lee </h3>
       </div>
       <div className="header__container-paragraphs">
-      <div className="header__container-paragraph">
-          Login{' '}
-      </div>
-      <div className="header__container-paragraph">
-          Profile
-      </div>
-      <div className="header__container-paragraph">
-       Form 
-      </div>
+        <div className="header__container-paragraph">Home</div>
+        <div className="header__container-paragraph">About</div>
+        <div className="header__container-paragraph">Projects</div>
+          <div className="header__container-paragraph">Contact</div>
       </div>
     </div>
   )
